23619dcc7bed590a534c83515603495664815d5c
Jonas Tiago <jonastiago95@gmail.com>
2022-11-02 16:43:52 -0300

    feat add cors for external server

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
-
+app.use(cors());
+const tts = [
+  {
+    username: "bobesponja",
+    avatar:
+      "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+    tweet: "eu amo o hub",
+  },
+  {
+    username: "bobesponja",
+    avatar:
+      "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+    tweet: "trietse demais com isso",
+  },
+  {
+    username: "bobesponja",
+    avatar:
+      "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+    tweet: "queria saber post",
+  },
+  {
+    username: "bobesponja",
+    avatar:
+      "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+    tweet: "partiu estudar post....afhhhhh",
+  },
+];
 app.get("/", (req, res) => {
-  res.send("ola");
+  res.send(tts);
 });
 
 app.listen(5000, () => console.log("Serve running port: 5000"));
