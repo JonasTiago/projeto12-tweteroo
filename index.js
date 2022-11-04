3badf138fc1788a720b5d0f2297219dad1c6721a
Jonas Tiago <jonastiago95@gmail.com>
2022-11-04 09:04:00 -0300

    feat post sign-up

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import cors from "cors";
 
 const app = express();
 app.use(cors());
-const tts = [
+app.use(express.json());
+
+const tweets = [
   {
     username: "bobesponja",
     avatar:
@@ -16,21 +18,20 @@ const tts = [
       "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
     tweet: "trietse demais com isso",
   },
-  {
-    username: "bobesponja",
-    avatar:
-      "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-    tweet: "queria saber post",
-  },
-  {
-    username: "bobesponja",
-    avatar:
-      "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-    tweet: "partiu estudar post....afhhhhh",
-  },
 ];
-app.get("/", (req, res) => {
-  res.send(tts);
+
+app.get("/tweets", (req, res) => {
+  res.send(tweets);
+});
+
+app.post("/sign-up", (req, res) => {
+  res.status(201).send("ok");
+});
+
+app.post("/tweets", (req, res) => {
+  const tweet = req.body;
+  tweets.push(tweet);
+  res.send("ok");
 });
 
 app.listen(5000, () => console.log("Serve running port: 5000"));
